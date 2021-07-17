cc9706e86407347e04506095920dd3f8ccf081bb
Wences81 <zhylinpochta@gmail.com>
2021-07-17 18:50:01 +0200

    js-hw12-day1

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -2,5 +2,6 @@ export default function getRefs() {
     return {
         inputSearch: document.getElementById("search-box"),
         countryInfo: document.querySelector(".country-info"),
+        countryList: document.querySelector(".country-list"),
     };
 }
